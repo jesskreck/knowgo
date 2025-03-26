e3097d3b16042a8799e565a0ad8b2d7a5238ba3c
jesskreck <krecker.jessica@gmail.com>
2025-03-26 08:35:46 +0100

    supabase config

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_SERVICE_KEY } from '$env/static/private';
+import { SUPABASE_ANON_KEY } from '$env/static/private';
 
 const supabaseUrl = "https://uqpnpkjnzqcvlraohkod.supabase.co";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabaseServiceKey = SUPABASE_SERVICE_KEY;
 
-
-
-export const supabase = createClient(supabaseUrl, supabaseServiceKey);
+export const supabase = createClient(supabaseUrl, SUPABASE_ANON_KEY);
